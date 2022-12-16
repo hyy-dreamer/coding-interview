3f811e580d0d372d7353e2b402164e059f6c654d
hyy-dreamer <yuyanghu@andrew.cmu.edu>
2022-12-16 15:29:22 -0500

    monotonic stack

---
+++ Monotonic_stack/src/Next_Greater_Element_496.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Next_Greater_Element_496 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2)
+    {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int[] ans = new int[nums1.length];
+        for(int num : nums2)
+        {
+            while(!st.isEmpty() && num > st.peek())
+            {
+                mp.put(st.pop(), num);
+            }
+            st.add(num);
+        }
+
+        for(int i = 0; i < nums1.length; i ++)
+        {
+            ans[i] = mp.getOrDefault(nums1[i], -1);
+        }
+        return ans;
+    }
+}
+//maintain a decreasing stack
